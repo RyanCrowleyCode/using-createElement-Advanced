fdef0f73581a18879ead7c568b91d4a69c9e0a11
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-14 14:01:19 -0500

    added messages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,26 @@ article.id = "messages"
 // Using appendChild(), attach each message as a child to the messages element.
 // Example output.
 
+const message1 = document.createElement('section')
+message1.className = "message"
+message1.textContent = "Hey how's it going?"
+
+const message2 = document.createElement('section')
+message2.className = "message"
+message2.textContent = "hey what's up. just working on this javascript"
+
+const message3 = document.createElement('section')
+message3.className = "message"
+message3.textContent = "Oh cool, me too. Which exercise are you working on?"
+
+const message4 = document.createElement('section')
+message4.className = "message"
+message4.textContent = "The advanced one from Chapter 2 book 7. It's the createElement one. Kind of struggling."
+
+const message5 = document.createElement('section')
+message5.className = "message"
+message5.textContent = "Oh yeah, that's a tough one! Hang in there, you'll get it."
+
 
 
 // <article id="messages">
