f049b02b361f4dd477a86ea5c0e0a25587f8f744
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-15 10:40:40 -0500

    Completed the advanced challenge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,12 @@
 // Using createElement(), you're going to create a simple list of chat messages that you might have with one of your family members. Maybe it's that wacky aunt that you see every Christmas and Fourth of July.
 
 // Put an article DOM element in your index.html with the id attribute value of messages.
+
+const myContainer = document.querySelector(".container")
 const article = document.createElement('article')
 article.id = "messages"
+myContainer.appendChild(article)
+
 
 // In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
 
@@ -12,25 +16,24 @@ article.id = "messages"
 // Using appendChild(), attach each message as a child to the messages element.
 // Example output.
 
+// creating elements for 5 messages
 const message1 = document.createElement('section')
-message1.className = "message"
-message1.textContent = "Hey how's it going?"
-
 const message2 = document.createElement('section')
-message2.className = "message"
-message2.textContent = "hey what's up. just working on this javascript"
-
 const message3 = document.createElement('section')
-message3.className = "message"
-message3.textContent = "Oh cool, me too. Which exercise are you working on?"
-
 const message4 = document.createElement('section')
-message4.className = "message"
-message4.textContent = "The advanced one from Chapter 2 book 7. It's the createElement one. Kind of struggling."
-
 const message5 = document.createElement('section')
-message5.className = "message"
-message5.textContent = "Oh yeah, that's a tough one! Hang in there, you'll get it."
+
+// a list of messages to cycle through
+const messageList = [message1, message2, message3, message4, message5]
+
+// assigning className of message to each message, adding the innerHTML with corresponding message number, appending final message to article
+for (let i = 0; i < messageList.length; i++) {
+    messageList[i].className = "message"
+    messageList[i].innerHTML = `This is message number ${i +1}`
+    article.appendChild(messageList[i])
+}
+
+
 
 
 
